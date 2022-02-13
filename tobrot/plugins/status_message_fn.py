a6586448b31d6ead3e63e97f0eddf8dede83a003
NoobdaPro <98887725+NoobdaPro@users.noreply.github.com>
2022-02-13 22:57:02 +0530

    Update status_message_fn.py

--- tobrot/plugins/status_message_fn.py
+++ tobrot/plugins/status_message_fn.py
@@ -92,7 +92,7 @@ async def status_message_f(
                 msg += f"\n<b>➥Status✓</b>: {file.progress_string()} <b>of</b> {file.total_length_string()}"
                 msg += f"\n<b>➥ETA⏳:</b> {file.eta_string()}"
                 
-                msg += f"\n{msgg}"
+                msg += f"\n➥{msgg}"
                 msg += f"\n<b>🗑GID:</b> <code>{file.gid}</code>"    
                 msg += "\n"
                 msg += f"<b>✥══ @StarterLeech ══✥</b>\n"
